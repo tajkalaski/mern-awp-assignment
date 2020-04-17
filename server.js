9c4c96bf73b65ba122a06256e2d6f4ecd677bf48
Thorben Luepkes <turbzcoding@gmail.com>
2020-04-17 19:30:32 +0200

    Fix me

--- server.js
+++ server.js
@@ -1,63 +1,65 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
 
 /**** Configuration ****/
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json()); // Parse JSON from the request body
-app.use(morgan('combined')); // Log all requests to the console
-app.use(express.static('../client/build')); // Needed for serving production build of React
-
+app.use(morgan("combined")); // Log all requests to the console
+app.use(express.static("../client/build")); // Needed for serving production build of React
 
 /**** Database ****/
-const questionDB = require('./question_db')(mongoose);
+const questionDB = require("./question_db")(mongoose);
 
 /**** Routes ****/
-app.get('/api/questions', async (req, res) => {
-    const questions = await questionDB.getQuestions();
-    res.json(questions);
+app.get("/api/questions", async (req, res) => {
+  const questions = await questionDB.getQuestions();
+  res.json(questions);
 });
 
-app.get('/api/questions/:id', async (req, res) => {
-    let id = req.params.id;
-    const question = await questionDB.getQuestion(id);
-    res.json(question);
+app.get("/api/questions/:id", async (req, res) => {
+  let id = req.params.id;
+  const question = await questionDB.getQuestion(id);
+  res.json(question);
 });
 
-app.post('/api/questions', async (req, res) => {
-    let question = {
-        question : req.body.name,
-        answers : [] // Empty hobby array
-    };
-    const newQuestion = await questionDB.createQuestion(question);
-    res.json(newQuestion);
+app.post("/api/questions", async (req, res) => {
+  let question = {
+    question: req.body.name,
+    answers: [], // Empty hobby array
+  };
+  const newQuestion = await questionDB.createQuestion(question);
+  res.json(newQuestion);
 });
 
-app.post('/api/questions/:id/answers', async (req, res) => {
-    const id = req.params.id;
-    const answer = req.body.hobby;
-    const updatedQuestion = await questionDB.addAnswer(id, answer);
-    res.json(updatedQuestion);
+app.post("/api/questions/:id/answers", async (req, res) => {
+  const id = req.params.id;
+  const answer = req.body.hobby;
+  const updatedQuestion = await questionDB.addAnswer(id, answer);
+  res.json(updatedQuestion);
 });
 
 // "Redirect" all get requests (except for the routes specified above) to React's entry point (index.html) to be handled by Reach router
 // It's important to specify this route as the very last one to prevent overriding all of the other routes
-app.get('*', (req, res) => {
-    // needed to modify this is bit since strucutre 
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  // needed to modify this is bit since strucutre
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 /**** Start ****/
-const url = process.env.MONGO_URL || 'mongodb+srv://tajsonik:836Ce881b@cluster0-23gsy.mongodb.net/question_db?retryWrites=true&w=majority';
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(async () => {
-        await questionDB.bootstrap(); // Fill in test data if needed.
-        await app.listen(port); // Start the API
-        console.log(`Question API running on port ${port}!`);
-    })
-    .catch(error => console.error(error));
+const url =
+  process.env.MONGO_URL ||
+  "mongodb+srv://tajsonik:836Ce881b@cluster0-23gsy.mongodb.net/question_db?retryWrites=true&w=majority";
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(async () => {
+    await questionDB.bootstrap(); // Fill in test data if needed.
+    await app.listen(port); // Start the API
+    console.log(`Question API running on port ${port}!`);
+  })
+  .catch((error) => console.error(error));
