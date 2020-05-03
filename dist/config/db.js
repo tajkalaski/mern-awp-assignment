7c61276c0fd1a15d5e961a90ecba75628b4cec85
Thorben Luepkes <turbzcoding@gmail.com>
2020-05-03 17:02:33 +0200

    no need for dist

--- dist/config/db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose = require("mongoose");
-/**** Database ****/
-exports.kittenDB = require("./question_db")(mongoose);
