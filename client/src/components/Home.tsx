58d003149776ece5a7219b9c1ee63ccf0b76a808
Thorben Luepkes <turbzcoding@gmail.com>
2020-05-05 19:31:47 +0200

    i cant code

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,14 +2,24 @@ import * as React from "react";
 import { Question, getQuestions } from "../api";
 
 export const Home = () => {
+  // first define states
+  const [questions, setQuestions] = React.useState<Question[]>([]);
+  // Then a effect for the initial mount
+  React.useEffect(() => {
+    loadQuestions();
+  }, []);
   const loadQuestions = () => {
     getQuestions().then((response) => {
       setQuestions(response);
     });
   };
-  const [questions, setQuestions] = React.useState<Question[]>();
-  React.useEffect(() => {
-    loadQuestions();
-  }, []);
-  return <div>Render a list of currently available questions lolololo</div>;
+  return (
+    <React.Fragment>
+      <div>
+        {questions.map((q) => (
+          <h1 key={q.text}>{q.text}</h1>
+        ))}
+      </div>
+    </React.Fragment>
+  );
 };
